e0f9574d7a45e2d93092bd12a25557e5410a867a
armaanpriyadarshan <armaan_priyadarshan@outlook.com>
2024-12-01 17:49:06 -0500

    optimization

--- 1/2.py
+++ 1/2.py
@@ -2,6 +2,7 @@ from collections import Counter
 
 with open('input.txt', 'r') as file:
     l1, l2 = zip(*(map(int, line.split()) for line in file))
-    
-similarity = sum(a * Counter(l2)[a] for a in l1)
+
+counter = Counter(l2)   
+similarity = sum(a * counter[a] for a in l1)
 print(similarity)
